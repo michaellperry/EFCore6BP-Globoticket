ee11f28438e81436acc9a411f2a51174db8cbff6
Michael L Perry <michael@qedcode.com>
2022-02-13 14:39:57 -0600

    Added PromotionService

---
+++ GloboTicket.Domain/Services/PromotionService.cs
@@ -0,0 +1,57 @@
+﻿using GloboTicket.Domain.Entities;
+
+namespace GloboTicket.Domain.Services;
+
+public class PromotionService
+{
+    private GloboTicketContext context;
+
+    public PromotionService(GloboTicketContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<Show> BookShow(Venue venue, Act act, DateTimeOffset date)
+    {
+        var show = new Show
+        {
+            Venue = venue,
+            Act = act,
+            Date = date
+        };
+
+        await context.AddAsync(show);
+        await context.SaveChangesAsync();
+
+        return show;
+    }
+
+    public async Task<Venue> CreateVenue(Guid venueGuid, string name, string address)
+    {
+        var venue = new Venue
+        {
+            VenueGuid = venueGuid,
+            Name = name,
+            Address = address
+        };
+
+        await context.AddAsync(venue);
+        await context.SaveChangesAsync();
+
+        return venue;
+    }
+
+    public async Task<Act> CreateAct(Guid actGuid, string name)
+    {
+        var act = new Act
+        {
+            ActGuid = actGuid,
+            Name = name
+        };
+
+        await context.AddAsync(act);
+        await context.SaveChangesAsync();
+
+        return act;
+    }
+}
