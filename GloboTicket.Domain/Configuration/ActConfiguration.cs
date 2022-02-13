130a46f8cb5bbd4c4e292bad62541cd80297735c
Michael L Perry <michael@qedcode.com>
2022-02-13 14:39:18 -0600

    Added Act and Show entities.

---
+++ GloboTicket.Domain/Configuration/ActConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using GloboTicket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GloboTicket.Domain.Configuration;
+
+internal class ActConfiguration : IEntityTypeConfiguration<Act>
+{
+    public void Configure(EntityTypeBuilder<Act> builder)
+    {
+        builder
+            .HasAlternateKey(a => a.ActGuid);
+        builder
+            .Property(a => a.Name)
+            .HasMaxLength(100);
+    }
+}
