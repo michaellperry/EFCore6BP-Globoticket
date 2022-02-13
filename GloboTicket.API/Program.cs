70fc5a369d014acb8a1f8d510c3fe76822ff4016
Michael L Perry <michael@qedcode.com>
2022-02-12 20:41:27 -0600

    Added API project

---
+++ GloboTicket.API/Program.cs
@@ -0,0 +1,30 @@
+using GloboTicket.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string connectionString = builder.Configuration
+    .GetConnectionString("GloboTicketConnection");
+builder.Services.AddInfrastructure(connectionString);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
