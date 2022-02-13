45507e00655a7a08e8b8eedde869ef92623cd86c
Michael L Perry <michael@qedcode.com>
2022-02-12 20:22:54 -0600

    Added Infrastructure project

---
+++ GloboTicket.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using GloboTicket.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.Infrastructure;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<GloboTicketContext>(options =>
+        {
+            options.UseSqlServer(connectionString, sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(
+                    typeof(ServiceRegistration).Assembly.FullName);
+            });
+        });
+
+        return services;
+    }
+}
