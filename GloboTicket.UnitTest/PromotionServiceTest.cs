187113291db2ec3257cfc6d467eb47e22443147d
Michael L Perry <michael@qedcode.com>
2022-02-13 14:44:42 -0600

    Added unit test project

---
+++ GloboTicket.UnitTest/PromotionServiceTest.cs
@@ -0,0 +1,19 @@
+using GloboTicket.Domain.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GloboTicket.UnitTest;
+
+public class PromotionServiceTest
+{
+    private readonly PromotionService promotionService;
+
+    public PromotionServiceTest()
+    {
+    }
+
+    [Fact]
+    public async Task CanBookAShow()
+    {
+    }
+}
