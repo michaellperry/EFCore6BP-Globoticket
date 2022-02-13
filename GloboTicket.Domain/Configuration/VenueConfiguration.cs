443f4e38e8bf34fb0d4bd1a85ed91709c1b65a55
Michael L Perry <michael@qedcode.com>
2022-02-12 20:57:42 -0600

    Added DbContext and Venue entity

---
+++ GloboTicket.Domain/Configuration/VenueConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GloboTicket.Domain.Configuration;
+
+public class VenueConfiguration : IEntityTypeConfiguration<Venue>
+{
+    public void Configure(EntityTypeBuilder<Venue> builder)
+    {
+        builder
+            .Property(v => v.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+        builder
+            .Property(v => v.Address)
+            .HasMaxLength(300);
+    }
+}
