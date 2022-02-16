19de8fc5e738c2c093502598b2afd237c9b0d1f6
Michael L Perry <michael@qedcode.com>
2022-02-15 19:39:20 -0600

    Test the promotion service.

---
+++ GloboTicket.UnitTest/TestModelConfiguration.cs
@@ -0,0 +1,11 @@
+using GloboTicket.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.UnitTest;
+
+internal class TestModelConfiguration : IModelConfiguration
+{
+    public void ConfigureModel(ModelBuilder modelBuilder)
+    {
+    }
+}
