efa1ed9a423fdaebb7e4a8c345c57f8839e5390a
Michael L Perry <michael@qedcode.com>
2022-02-15 20:58:16 -0600

    Added DbContext and Venue entity

---
+++ GloboTicket.Domain/GloboTicketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.Domain;
+public class GloboTicketContext : DbContext
+{
+    public GloboTicketContext(DbContextOptions<GloboTicketContext> options) :
+        base(options)
+    {
+    }
+
+    public DbSet<Venue> Venue { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(GloboTicketContext).Assembly);
+    }
+}
