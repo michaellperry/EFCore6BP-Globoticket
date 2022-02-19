4532288f9b4b0dbbf3bd69e7c97843bd3aba4ab2
Michael L Perry <michael@qedcode.com>
2022-02-19 13:29:41 -0600

    Apply SQL model configuration

--- GloboTicket.Domain/GloboTicketContext.cs
+++ GloboTicket.Domain/GloboTicketContext.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GloboTicket.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace GloboTicket.Domain;
 public class GloboTicketContext : DbContext
 {
-    public GloboTicketContext(DbContextOptions<GloboTicketContext> options) :
+    private readonly IModelConfiguration modelConfiguration;
+
+    public GloboTicketContext(DbContextOptions<GloboTicketContext> options, IModelConfiguration modelConfiguration) :
         base(options)
     {
+        this.modelConfiguration = modelConfiguration;
     }
 
     public DbSet<Venue> Venue { get; set; }
@@ -13,5 +17,6 @@ public class GloboTicketContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(GloboTicketContext).Assembly);
+        modelConfiguration.ConfigureModel(modelBuilder);
     }
 }
