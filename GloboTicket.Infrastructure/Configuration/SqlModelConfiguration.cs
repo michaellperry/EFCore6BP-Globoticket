a11f618b174dc00b609c0b98318c9d6402f87ca8
Michael L Perry <michael@qedcode.com>
2022-02-19 13:29:41 -0600

    Apply SQL model configuration

---
+++ GloboTicket.Infrastructure/Configuration/SqlModelConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using GloboTicket.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.Infrastructure.Configuration;
+
+internal class SqlModelConfiguration : IModelConfiguration
+{
+    public void ConfigureModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqlModelConfiguration).Assembly);
+    }
+}
