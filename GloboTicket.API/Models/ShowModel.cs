5f91dc576a93ade76a122f09609320637063efbb
Michael L Perry <michael@qedcode.com>
2022-03-06 17:07:53 -0600

    Add shows controller

---
+++ GloboTicket.API/Models/ShowModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GloboTicket.API.Models
+{
+    public class ShowModel
+    {
+        public string ActName { get; set; } = "";
+        public string VenueName { get; set; } = "";
+        public string VenueAddress { get; set; } = "";
+        public DateTimeOffset Date { get; set; }
+    }
+}
