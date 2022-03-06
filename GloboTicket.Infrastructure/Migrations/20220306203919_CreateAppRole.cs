c80138db983d21a22ac2f93a7eebf8af02ddd3dc
Michael L Perry <michael@qedcode.com>
2022-03-06 15:52:25 -0600

    Create the app role

---
+++ GloboTicket.Infrastructure/Migrations/20220306203919_CreateAppRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GloboTicket.Infrastructure.Migrations
+{
+    public partial class CreateAppRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE ROLE globoticket_app AUTHORIZATION db_securityadmin");
+            migrationBuilder.Sql("ALTER ROLE db_datareader ADD MEMBER globoticket_app");
+            migrationBuilder.Sql("ALTER ROLE db_datawriter ADD MEMBER globoticket_app");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER ROLE db_datawriter DROP MEMBER globoticket_app");
+            migrationBuilder.Sql("ALTER ROLE db_datareader DROP MEMBER globoticket_app");
+            migrationBuilder.Sql("DROP ROLE globoticket_app");
+        }
+    }
+}
