630ba2bc65b9bcf64554f720f2ef914d2903d470
Michael L Perry <michael@qedcode.com>
2022-03-06 11:10:59 -0600

    Add design time DbContext factory

---
+++ GloboTicket.API/DesignTime/DesignTimeModelConfiguration.cs
@@ -0,0 +1,11 @@
+using GloboTicket.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.API.DesignTime;
+
+internal class DesignTimeModelConfiguration : IModelConfiguration
+{
+    public void ConfigureModel(ModelBuilder modelBuilder)
+    {
+    }
+}
