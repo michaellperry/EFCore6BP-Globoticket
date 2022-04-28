7ce1fa22e41cd5fd2f1e36a0474fb7e8b5eeb23a
Michael L Perry <michael@qedcode.com>
2022-04-27 20:35:23 -0500

    Extract query class

---
+++ GloboTicket.API/Queries/ListShowsQuery.cs
@@ -0,0 +1,29 @@
+﻿using GloboTicket.API.Models;
+using GloboTicket.Domain;
+using GloboTicket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.API.Queries;
+
+public class ListShowsQuery
+{
+    private readonly GloboTicketContext context;
+
+    public ListShowsQuery(GloboTicketContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<List<ShowModel>> Execute()
+    {
+        return await context.Set<Show>()
+            .Select(show => new ShowModel
+            {
+                ActName = show.Act.Name,
+                VenueName = show.Venue.Name,
+                VenueAddress = show.Venue.Address,
+                Date = show.Date
+            })
+            .ToListAsync();
+    }
+}
