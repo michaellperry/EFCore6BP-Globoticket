bc64244a0bd2c7231db37f01e7ceb1061fd1f365
Michael L Perry <michael@qedcode.com>
2022-04-27 20:35:23 -0500

    Extract query class

--- GloboTicket.API/Controllers/ShowsController.cs
+++ GloboTicket.API/Controllers/ShowsController.cs
@@ -1,33 +1,24 @@
 ﻿using GloboTicket.API.Models;
-using GloboTicket.Domain;
-using GloboTicket.Domain.Entities;
+using GloboTicket.API.Queries;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GloboTicket.API.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class ShowsController : ControllerBase
 {
-    private readonly GloboTicketContext context;
+    private readonly ListShowsQuery listShowsQuery;
 
-    public ShowsController(GloboTicketContext context)
+    public ShowsController(ListShowsQuery listShowsQuery)
     {
-        this.context = context;
+        this.listShowsQuery = listShowsQuery;
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<ShowModel>>> GetShows()
     {
-        var shows = await context.Set<Show>()
-            .Select(show => new ShowModel
-            {
-                ActName = show.Act.Name,
-                VenueName = show.Venue.Name,
-                VenueAddress = show.Venue.Address,
-                Date = show.Date
-            })
-            .ToListAsync();
+        List<ShowModel> shows = await listShowsQuery.Execute();
         return Ok(shows);
     }
 }
