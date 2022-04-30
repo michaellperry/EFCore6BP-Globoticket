b8d351c9e8ea6dc7872e4cca69e6d8e9915dc1d8
Michael L Perry <michael@qedcode.com>
2022-04-30 18:14:57 -0500

    Add migration for venue location

---
+++ GloboTicket.Infrastructure/Migrations/20220430231252_AddVenueLocation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace GloboTicket.Infrastructure.Migrations
+{
+    public partial class AddVenueLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Venue",
+                type: "geography",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Venue");
+        }
+    }
+}
