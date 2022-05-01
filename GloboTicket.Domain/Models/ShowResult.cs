20b06aff3160660ff337cf476f6c103b34caf9db
Michael L Perry <michael@qedcode.com>
2022-05-01 12:13:00 -0500

    Separate latitude and longitude

--- GloboTicket.Domain/Models/ShowResult.cs
+++ GloboTicket.Domain/Models/ShowResult.cs
@@ -7,7 +7,8 @@ public class ShowResult
     public string HrefShow { get; set; } = "";
     public string VenueName { get; set; } = "";
     public string? VenueAddress { get; set; }
-    public Point? VenueLocation { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
     public double? Distance { get; set; }
     public string ActName { get; set; } = "";
     public DateTimeOffset Date { get; set; }
