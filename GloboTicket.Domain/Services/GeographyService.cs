1324024a97bcce0e19518dd9cb25bbb1977fb8ed
Michael L Perry <michael@qedcode.com>
2022-04-30 22:29:55 -0500

    Create a geographic location

---
+++ GloboTicket.Domain/Services/GeographyService.cs
@@ -0,0 +1,11 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace GloboTicket.Domain.Services;
+
+public static class GeographyService
+{
+    public static Point GeographicLocation(double latitude, double longitude)
+    {
+        return new Point(longitude, latitude) { SRID = 4326 };
+    }
+}
