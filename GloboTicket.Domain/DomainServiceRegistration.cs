886d0d40a9ef3cd562d1a774d51c0daf96220dfa
Michael L Perry <michael@qedcode.com>
2022-05-01 08:22:45 -0500

    Added sales service

--- GloboTicket.Domain/DomainServiceRegistration.cs
+++ GloboTicket.Domain/DomainServiceRegistration.cs
@@ -8,6 +8,7 @@ public static class DomainServiceRegistration
     public static IServiceCollection AddDomain(this IServiceCollection services)
     {
         services.AddScoped<PromotionService>();
+        services.AddScoped<SalesService>();
         return services;
     }
 }
