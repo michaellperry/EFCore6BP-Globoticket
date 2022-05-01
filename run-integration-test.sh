f4c04e960a78b63c477255ea052c0a963fe7f3af
Michael L Perry <michael@qedcode.com>
2022-05-01 11:33:13 -0500

    Fail on build error. Why is this not the default?

--- run-integration-test.sh
+++ run-integration-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 docker build -t globoticket/integration-test:latest -f IntegrationTest/IntegrationTest.Dockerfile .
 docker compose -f IntegrationTest/docker-compose.yaml up --exit-code-from test --renew-anon-volumes
 docker compose -f IntegrationTest/docker-compose.yaml down -v
