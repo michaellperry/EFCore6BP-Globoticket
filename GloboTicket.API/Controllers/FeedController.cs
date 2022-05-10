73b31991e378767b30c6e08df64d1d535d139924
Michael L Perry <michael@qedcode.com>
2022-05-09 21:17:01 -0500

    Separate the feed service from the controller

--- GloboTicket.API/Controllers/FeedController.cs
+++ GloboTicket.API/Controllers/FeedController.cs
@@ -1,7 +1,6 @@
-﻿using GloboTicket.Domain;
-using GloboTicket.Domain.Entities;
+﻿using GloboTicket.Domain.Models;
+using GloboTicket.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GloboTicket.API.Controllers;
 
@@ -9,85 +8,18 @@ namespace GloboTicket.API.Controllers;
 [ApiController]
 public class FeedController : Controller
 {
-    private readonly GloboTicketContext context;
+    private readonly FeedService feedService;
 
-    public FeedController(GloboTicketContext context)
+    public FeedController(FeedService feedService)
     {
-        this.context = context;
+        this.feedService = feedService;
     }
 
     [HttpGet]
     [Route("shows")]
-    public ActionResult<IAsyncEnumerable<ShowInfo>> ShowsFeed()
+    public async Task<ActionResult<List<ShowInfo>>> ShowsFeed()
     {
-        var shows = context.Set<Show>()
-            .Select(show => new ShowInfo(
-                show.ShowGuid,
-                show.Act.ActGuid,
-                show.Venue.VenueGuid,
-                show.Date
-            ))
-            .AsAsyncEnumerable();
+        var shows = await feedService.ListShows();
         return Ok(shows);
-    }
-
-    [HttpGet]
-    [Route("list")]
-    public ActionResult<List<ShowInfo>> ShowList()
-    {
-        var shows = context.Set<Show>()
-            .Select(show => new ShowInfo(
-                show.ShowGuid,
-                show.Act.ActGuid,
-                show.Venue.VenueGuid,
-                show.Date
-            ))
-            .ToList();
-        return Ok(shows);
-    }
-
-    [HttpGet]
-    [Route("enumerable")]
-    public ActionResult<IEnumerable<ShowInfo>> ShowEnumerable()
-    {
-        var shows = context.Set<Show>()
-            .Select(show => new ShowInfo(
-                show.ShowGuid,
-                show.Act.ActGuid,
-                show.Venue.VenueGuid,
-                show.Date
-            ))
-            .AsEnumerable();
-        return Ok(shows);
-    }
-}
-
-public class ShowInfo
-{
-    public Guid ShowGuid { get; }
-    public Guid ActGuid { get; }
-    public Guid VenueGuid { get; }
-    public DateTimeOffset Date { get; }
-
-    public ShowInfo(Guid showGuid, Guid actGuid, Guid venueGuid, DateTimeOffset date)
-    {
-        ShowGuid = showGuid;
-        ActGuid = actGuid;
-        VenueGuid = venueGuid;
-        Date = date;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is ShowInfo other &&
-               ShowGuid.Equals(other.ShowGuid) &&
-               ActGuid.Equals(other.ActGuid) &&
-               VenueGuid.Equals(other.VenueGuid) &&
-               Date.Equals(other.Date);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(ShowGuid, ActGuid, VenueGuid, Date);
     }
 }
