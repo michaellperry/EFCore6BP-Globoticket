e5d53df0dad5b37d19e800f9c7e903308e155108
Michael L Perry <michael@qedcode.com>
2022-05-09 20:59:28 -0500

    Try different solutions to the feed controller

---
+++ GloboTicket.API/Controllers/FeedController.cs
@@ -0,0 +1,93 @@
+﻿using GloboTicket.Domain;
+using GloboTicket.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FeedController : Controller
+{
+    private readonly GloboTicketContext context;
+
+    public FeedController(GloboTicketContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    [Route("shows")]
+    public ActionResult<IAsyncEnumerable<ShowInfo>> ShowsFeed()
+    {
+        var shows = context.Set<Show>()
+            .Select(show => new ShowInfo(
+                show.ShowGuid,
+                show.Act.ActGuid,
+                show.Venue.VenueGuid,
+                show.Date
+            ))
+            .AsAsyncEnumerable();
+        return Ok(shows);
+    }
+
+    [HttpGet]
+    [Route("list")]
+    public ActionResult<List<ShowInfo>> ShowList()
+    {
+        var shows = context.Set<Show>()
+            .Select(show => new ShowInfo(
+                show.ShowGuid,
+                show.Act.ActGuid,
+                show.Venue.VenueGuid,
+                show.Date
+            ))
+            .ToList();
+        return Ok(shows);
+    }
+
+    [HttpGet]
+    [Route("enumerable")]
+    public ActionResult<IEnumerable<ShowInfo>> ShowEnumerable()
+    {
+        var shows = context.Set<Show>()
+            .Select(show => new ShowInfo(
+                show.ShowGuid,
+                show.Act.ActGuid,
+                show.Venue.VenueGuid,
+                show.Date
+            ))
+            .AsEnumerable();
+        return Ok(shows);
+    }
+}
+
+public class ShowInfo
+{
+    public Guid ShowGuid { get; }
+    public Guid ActGuid { get; }
+    public Guid VenueGuid { get; }
+    public DateTimeOffset Date { get; }
+
+    public ShowInfo(Guid showGuid, Guid actGuid, Guid venueGuid, DateTimeOffset date)
+    {
+        ShowGuid = showGuid;
+        ActGuid = actGuid;
+        VenueGuid = venueGuid;
+        Date = date;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ShowInfo other &&
+               ShowGuid.Equals(other.ShowGuid) &&
+               ActGuid.Equals(other.ActGuid) &&
+               VenueGuid.Equals(other.VenueGuid) &&
+               Date.Equals(other.Date);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ShowGuid, ActGuid, VenueGuid, Date);
+    }
+}
