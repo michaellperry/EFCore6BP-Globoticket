03f20417c533c01e97b15ff6846d3676a7159fad
Michael L Perry <michael@qedcode.com>
2022-05-09 21:17:01 -0500

    Separate the feed service from the controller

---
+++ GloboTicket.Domain/Services/FeedService.cs
@@ -0,0 +1,29 @@
+﻿using GloboTicket.Domain.Entities;
+using GloboTicket.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.Domain.Services;
+
+public class FeedService
+{
+    private readonly GloboTicketContext context;
+
+    public FeedService(GloboTicketContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<List<ShowInfo>> ListShows()
+    {
+        var shows = await context.Set<Show>()
+            .Select(show => new ShowInfo
+            {
+                ShowGuid = show.ShowGuid,
+                ActGuid = show.Act.ActGuid,
+                VenueGuid = show.Venue.VenueGuid,
+                Date = show.Date
+            })
+            .ToListAsync();
+        return shows;
+    }
+}
