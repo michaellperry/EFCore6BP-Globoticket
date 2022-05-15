76cb877f1202e1a4576128c44ebb82d535d5a74a
Michael L Perry <michael@qedcode.com>
2022-05-15 14:49:49 -0500

    Add venue show and act endpoints

---
+++ GloboTicket.API/Controllers/VenuesController.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.API.Controllers;
+
+public class VenuesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("{venueGuid}")]
+    public async Task<ActionResult<VenueModel>> GetVenue([FromRoute] Guid venueGuid)
+    {
+        throw new NotImplementedException();
+    }
+}
