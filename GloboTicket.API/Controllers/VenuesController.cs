fdfc577976419e94487176c36d4cef8dd40b30ff
Michael L Perry <michael@qedcode.com>
2022-12-25 12:30:06 -0600

    Added attributes to Acts and Venues controllers. This allows Swagger to document the controllers.

--- GloboTicket.API/Controllers/VenuesController.cs
+++ GloboTicket.API/Controllers/VenuesController.cs
@@ -3,13 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GloboTicket.API.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class VenuesController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
-
     [HttpGet]
     [Route("{venueGuid}")]
     public async Task<ActionResult<VenueModel>> GetVenue([FromRoute] Guid venueGuid)
